7e307aa6dfad9e35c15adc6d2acf876f34a109ff
Noj Vek <noj.vek@mixpanel.com>
2017-08-16 18:25:32 -0700

    v1 of panel extension

---
+++ chrome_extension/devtools.js
@@ -0,0 +1,20 @@
+// The function below is executed in the context of the inspected page.
+var panel_getState = function() {
+  if ($0 && $0.update && $0.state) {
+    $0.update(); // Force a refresh so we can set state and see an update after a re-select
+    return $0.state;
+  }
+  return {error: "No state found"};
+}
+
+chrome.devtools.panels.elements.createSidebarPane("Panel State", function(sidebar) {
+  function updateElementProperties() {
+    // setExpression just shows result of a nested expression that is editable
+    // Chrome doesn't expose an API to detect when the result is changed by manual editing
+    // The work around is to unselect the element and re-select it so $0.update() is called
+    sidebar.setExpression("(" + panel_getState.toString() + ")()");
+  }
+
+  chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementProperties);
+  updateElementProperties();
+});
