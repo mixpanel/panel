009032529f4ad4bf42eec81225961160b5adcb6c
Noj Vek <noj.vek@mixpanel.com>
2017-09-26 13:54:42 -0700

    mocha use function rather than arrow functions

--- test/browser/component-tests.js
+++ test/browser/component-tests.js
@@ -2,43 +2,43 @@
 /* global sinon, expect */
 /* eslint no-unused-expressions:0 */
 
-describe(`Simple Component instance`, () => {
+describe(`Simple Component instance`, function() {
   let el;
 
-  beforeEach(() => {
+  beforeEach(function() {
     document.body.innerHTML = ``;
     el = document.createElement(`simple-app`);
   });
 
-  describe(`toString()`, () => {
-    it(`includes the tag name`, () => {
+  describe(`toString()`, function() {
+    it(`includes the tag name`, function() {
       expect(el.toString()).to.contain(`SIMPLE-APP`);
     });
   });
 
-  describe(`panelID`, () => {
-    it(`is unique for each component instance`, () => {
+  describe(`panelID`, function() {
+    it(`is unique for each component instance`, function() {
       const ids = Array(5).fill()
-        .map(() =>   { return document.createElement(`simple-app`); })
-        .map(function(el) { return el.panelID; });
+        .map(() => document.createElement(`simple-app`))
+        .map(el => el.panelID);
       expect(ids).to.have.lengthOf(new Set(ids).size);
     });
   });
 
-  describe(`config`, () => {
-    it(`returns config object via getter`, () => {
+  describe(`config`, function() {
+    it(`returns config object via getter`, function() {
       expect(el.config).to.be.an.instanceof(Object);
       expect(el.config.helpers).to.be.an.instanceof(Object);
     });
 
-    it(`exposes helpers via a top-level getter`, () => {
+    it(`exposes helpers via a top-level getter`, function() {
       expect(el.helpers).to.be.an.instanceof(Object);
       expect(el.helpers.capitalize).to.be.an.instanceof(Function);
     });
   });
 
-  context(`before attached to DOM`, () => {
-    it(`does not affect the DOM`, done => {
+  context(`before attached to DOM`, function() {
+    it(`does not affect the DOM`, function(done) {
       expect(document.getElementsByClassName(`foo`)).to.be.empty;
       window.requestAnimationFrame(() => {
         expect(document.getElementsByClassName(`foo`)).to.be.empty;
@@ -46,7 +46,7 @@ describe(`Simple Component instance`, () => {
       });
     });
 
-    it(`allows state setting`, done => {
+    it(`allows state setting`, function(done) {
       el.state = {foo: `not bar`};
       document.body.appendChild(el);
       expect(el.state.foo).to.equal(`not bar`);
@@ -56,7 +56,7 @@ describe(`Simple Component instance`, () => {
       });
     });
 
-    it(`allows updates and applies them when attached`, done => {
+    it(`allows updates and applies them when attached`, function(done) {
       el.update({foo: `not bar`});
       document.body.appendChild(el);
       expect(el.state.foo).to.equal(`not bar`);
@@ -68,7 +68,7 @@ describe(`Simple Component instance`, () => {
       });
     });
 
-    it(`caches the last template once rendered`, done => {
+    it(`caches the last template once rendered`, function(done) {
       expect(el._rendered).to.be.undefined;
       document.body.appendChild(el);
       window.requestAnimationFrame(() => {
@@ -78,29 +78,29 @@ describe(`Simple Component instance`, () => {
     });
   });
 
-  context(`when attached to DOM`, () => {
-    beforeEach(done => {
+  context(`when attached to DOM`, function() {
+    beforeEach(function(done) {
       document.body.appendChild(el);
       window.requestAnimationFrame(() => {
         done();
       });
     });
 
-    it(`renders its template`, () => {
+    it(`renders its template`, function() {
       expect(document.getElementsByClassName(`foo`)).to.have.lengthOf(1);
       expect(el.children).to.have.lengthOf(1);
       expect(el.children[0].className).to.equal(`foo`);
     });
 
-    it(`injects default state into templates`, () => {
+    it(`injects default state into templates`, function() {
       expect(el.textContent).to.contain(`Value of foo: bar`);
     });
 
-    it(`injects helpers into templates`, () => {
+    it(`injects helpers into templates`, function() {
       expect(el.textContent).to.contain(`Foo capitalized: Bar`);
     });
 
-    it(`re-renders when state is updated`, done => {
+    it(`re-renders when state is updated`, function(done) {
       expect(el.textContent).to.contain(`Value of foo: bar`);
       expect(el.textContent).to.contain(`Foo capitalized: Bar`);
       el.update({foo: `new value`});
@@ -111,7 +111,7 @@ describe(`Simple Component instance`, () => {
       });
     });
 
-    it(`does not re-render if shouldUpdate() returns false`, done => {
+    it(`does not re-render if shouldUpdate() returns false`, function(done) {
       expect(el.textContent).to.contain(`Value of foo: bar`);
 
       el.update({foo: `meow`});
@@ -127,8 +127,8 @@ describe(`Simple Component instance`, () => {
     });
   });
 
-  context(`when using shadow DOM`, () => {
-    beforeEach(done => {
+  context(`when using shadow DOM`, function() {
+    beforeEach(function(done) {
       el = document.createElement(`shadow-dom-app`);
       document.body.appendChild(el);
       window.requestAnimationFrame(() => {
@@ -136,12 +136,12 @@ describe(`Simple Component instance`, () => {
       });
     });
 
-    it(`creates and uses a shadow root`, () => {
+    it(`creates and uses a shadow root`, function() {
       expect(el.el).not.to.equal(el);
       expect(el.shadowRoot).to.be.ok;
     });
 
-    it(`successfully finds the panel root when top level uses shadow dom`, done => {
+    it(`successfully finds the panel root when top level uses shadow dom`, function(done) {
       const childEl = document.createElement(`nested-child`);
       window.requestAnimationFrame(() => {
         childEl.$panelParentID = el.panelID;
@@ -154,7 +154,7 @@ describe(`Simple Component instance`, () => {
       });
     });
 
-    it(`successfully finds the panel root when a nested child uses shadow dom`, done => {
+    it(`successfully finds the panel root when a nested child uses shadow dom`, function(done) {
       const rootEl = document.createElement(`nested-app`);
       document.body.appendChild(rootEl);
       window.requestAnimationFrame(() => {
@@ -174,18 +174,18 @@ describe(`Simple Component instance`, () => {
       });
     });
 
-    it(`renders its template`, () => {
+    it(`renders its template`, function() {
       expect(document.getElementsByClassName(`foo`)).to.have.lengthOf(0);
       expect(el.children).to.have.lengthOf(0);
       expect(el.shadowRoot.children[1].className).to.equal(`foo`);
     });
 
-    it(`applies the styles`, () => {
+    it(`applies the styles`, function() {
       expect(el.shadowRoot.children[0].innerHTML).to.equal(`color: blue;`);
     });
 
-    context(`when applying override styles`, () => {
-      it(`appends the overriding styles to the default styles`, done => {
+    context(`when applying override styles`, function() {
+      it(`appends the overriding styles to the default styles`, function(done) {
         el.setAttribute(`style-override`, `background: red;`);
         window.requestAnimationFrame(() => {
           expect(el.shadowRoot.children[0].innerHTML).to.equal(`color: blue;background: red;`);
@@ -193,7 +193,7 @@ describe(`Simple Component instance`, () => {
         });
       });
 
-      it(`it applies the styles even if the component isn't attached to the DOM`, () => {
+      it(`it applies the styles even if the component isn't attached to the DOM`, function() {
         el = document.createElement(`shadow-dom-app`);
         el.setAttribute(`style-override`, `background: red;`);
         expect(el.shadowRoot.children[0].innerHTML).to.equal(`color: blue;background: red;`);
@@ -202,17 +202,17 @@ describe(`Simple Component instance`, () => {
   });
 });
 
-describe(`Nested Component instance`, () => {
+describe(`Nested Component instance`, function() {
   let el, childEl;
 
-  context(`before child is rendered`, () => {
-    beforeEach(() => {
+  context(`before child is rendered`, function() {
+    beforeEach(function() {
       document.body.innerHTML = ``;
       childEl = null;
       el = document.createElement(`nested-app`);
     });
 
-    it(`successfully finds the panel root`, done => {
+    it(`successfully finds the panel root`, function(done) {
       document.body.appendChild(el);
       window.requestAnimationFrame(() => {
         childEl = document.createElement(`nested-child`);
@@ -225,7 +225,7 @@ describe(`Nested Component instance`, () => {
       });
     });
 
-    it(`successfully finds a panel parent node by a given tag name`, done => {
+    it(`successfully finds a panel parent node by a given tag name`, function(done) {
       document.body.appendChild(el);
       window.requestAnimationFrame(() => {
         childEl = document.createElement(`nested-child`);
@@ -238,7 +238,7 @@ describe(`Nested Component instance`, () => {
       });
     });
 
-    it(`passes state updates from child to parent`, () => {
+    it(`passes state updates from child to parent`, function() {
       el.attachedCallback();
       childEl = document.createElement(`nested-child`);
       childEl.$panelParentID = el.panelID;
@@ -249,8 +249,8 @@ describe(`Nested Component instance`, () => {
     });
   });
 
-  context(`when attached to DOM`, () => {
-    beforeEach(done => {
+  context(`when attached to DOM`, function() {
+    beforeEach(function(done) {
       document.body.innerHTML = ``;
       el = document.createElement(`nested-app`);
       document.body.appendChild(el);
@@ -260,26 +260,26 @@ describe(`Nested Component instance`, () => {
       });
     });
 
-    it(`renders the parent component`, () => {
+    it(`renders the parent component`, function() {
       expect(document.getElementsByClassName(`nested-foo`)).to.have.lengthOf(1);
       expect(el.children).to.have.lengthOf(1);
       expect(el.children[0].className).to.equal(`nested-foo`);
     });
 
-    it(`renders the child component`, () => {
+    it(`renders the child component`, function() {
       expect(document.getElementsByClassName(`nested-foo-child`)).to.have.lengthOf(1);
       expect(childEl.children[0].className).to.equal(`nested-foo-child`);
     });
 
-    it(`passes parent state to the child component`, () => {
+    it(`passes parent state to the child component`, function() {
       expect(childEl.textContent).to.include(`parent title: test`);
     });
 
-    it(`passes attributes to the child component`, () => {
+    it(`passes attributes to the child component`, function() {
       expect(childEl.textContent).to.include(`animal: llama`);
     });
 
-    it(`passes state updates from parent to child`, done => {
+    it(`passes state updates from parent to child`, function(done) {
       expect(childEl.textContent).to.include(`animal: llama`);
       el.update({animal: `capybara`});
       window.requestAnimationFrame(() => {
@@ -288,7 +288,7 @@ describe(`Nested Component instance`, () => {
       });
     });
 
-    it(`passes state updates from child to parent`, done => {
+    it(`passes state updates from child to parent`, function(done) {
       expect(el.textContent).to.include(`Nested app: test`);
       expect(childEl.textContent).to.include(`parent title: test`);
       childEl.update({title: `new title`});
@@ -302,10 +302,10 @@ describe(`Nested Component instance`, () => {
 });
 
 
-describe(`Rendering exception`, () => {
+describe(`Rendering exception`, function() {
   let el;
 
-  beforeEach(done => {
+  beforeEach(function(done) {
     document.body.innerHTML = ``;
     el = document.createElement(`breakable-app`);
     el.logError = sinon.spy();
@@ -315,16 +315,16 @@ describe(`Rendering exception`, () => {
     });
   });
 
-  it(`does not prevent component from initializing`, () => {
+  it(`does not prevent component from initializing`, function() {
     expect(el.initialized).to.be.ok;
   });
 
-  it(`logs an error`, () => {
+  it(`logs an error`, function() {
     expect(el.logError.getCall(0).args[0]).to.contain(`Error while rendering`);
     expect(el.logError.getCall(0).args[0]).to.contain(`BREAKABLE-APP`);
   });
 
-  it(`does not prevent further updates from rendering`, done => {
+  it(`does not prevent further updates from rendering`, function(done) {
     expect(el.textContent).not.to.contain(`Value of foo.bar`);
     el.update({foo: {bar: `later success`}});
     window.requestAnimationFrame(() => {
@@ -334,21 +334,21 @@ describe(`Rendering exception`, () => {
   });
 });
 
-describe(`Controlled App`, () => {
+describe(`Controlled App`, function() {
   let el;
 
-  beforeEach(done => {
+  beforeEach(function(done) {
     document.body.innerHTML = ``;
     el = document.createElement(`controlled-app`);
     document.body.appendChild(el);
     window.requestAnimationFrame(() => done());
   });
 
-  it(`does not allow update on component`, () => {
+  it(`does not allow update on component`, function() {
     expect(() => el.update({foo: `not bar`})).to.throw(/update\(\) not allowed from component. Use controller/);
   });
 
-  it(`Behaves like normal component`, done => {
+  it(`Behaves like normal component`, function(done) {
     let count = 0;
     expect(el.controller.state).to.be.eql({count});
     expect(el.textContent).to.contain(`Counter: ${count}`);
