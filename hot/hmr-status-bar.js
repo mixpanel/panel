a0a4d6f2da5485beb7a8d435e08fd693d1a0a349
Noj Vek <noj.vek@mixpanel.com>
2018-02-01 10:58:37 -0800

    PR feedback from jalack

--- hot/hmr-status-bar.js
+++ hot/hmr-status-bar.js
@@ -5,20 +5,20 @@
  */
 
 const webpackEventColors = {
-  ok: `39d183`, // green (connected and idle)
-  invalid: `a081ea`, // purple (compiling)
-  warnings: `dd731d`, // orange
-  errors: `e4567b`, // red
-  close: `9bacbf`, // grey (socket disconnected)
+  ok: `#39d183`, // green (connected and idle)
+  invalid: `#a081ea`, // purple (compiling)
+  warnings: `#dd731d`, // orange
+  errors: `#e4567b`, // red
+  close: `#9bacbf`, // grey (socket disconnected)
 };
 
 window.addEventListener(`message`, event => {
   const webpackPrefix = `webpack`;
-  let eventType = event.data.type;
+  const eventType = event.data.type;
   if (eventType && eventType.startsWith(webpackPrefix)) {
-    eventType = eventType.substr(webpackPrefix.length).toLowerCase();
+    const webpackEvent = eventType.substr(webpackPrefix.length).toLowerCase();
     const bodyStyle = window.document.body.style;
-    bodyStyle.borderTop = `2px solid #${webpackEventColors[eventType]}`;
+    bodyStyle.borderTop = `2px solid ${webpackEventColors[webpackEvent]}`;
   }
 });
 
