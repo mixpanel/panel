478a1c4f2a8959aea1feb9deb4662d16a8868a08
Noj Vek <noj.vek@mixpanel.com>
2018-06-12 22:34:47 -0700

    Ted's feedback

--- test/fixtures/attr-reflection-app.js
+++ test/fixtures/attr-reflection-app.js
@@ -3,8 +3,7 @@ import {Component, h} from '../../lib';
 export class AttrReflectionApp extends Component {
   get config() {
     return {
-      // eslint-disable-next-line no-unused-vars
-      template: state => h(`div`, {class: {'attr-app': true}}, [
+      template: () => h(`div`, {class: {'attr-app': true}}, [
         h(`p`, `Value of attribute wombats: ${this.getAttribute(`wombats`)}`),
       ]),
     };
--- test/fixtures/shadow-dom-app.js
+++ test/fixtures/shadow-dom-app.js
@@ -4,8 +4,7 @@ export class ShadowDomApp extends Component {
   get config() {
     return {
       css: `color: blue;`,
-      // eslint-disable-next-line no-unused-vars
-      template: state => h(`div`, {class: {foo: true}}, [
+      template: () => h(`div`, {class: {foo: true}}, [
         h(`p`, `Hello`),
       ]),
 
