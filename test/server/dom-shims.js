32aa0cf9e9c0a4f2947bd3fdf59df2fab027f97e
Noj Vek <noj.vek@mixpanel.com>
2019-03-04 10:13:01 -0800

    ted tweaks

--- test/server/dom-shims.js
+++ test/server/dom-shims.js
@@ -8,12 +8,15 @@ import {SimpleApp} from '../fixtures/simple-app';
 customElements.define(`my-app`, SimpleApp);
 
 describe(`customElements registry`, function() {
-  it(`.get() returns component class`, function() {
-    expect(customElements.get(`my-app`)).to.eql(SimpleApp);
+  describe(`.get()`, function() {
+    it(`returns component class`, function() {
+      expect(customElements.get(`my-app`)).to.eql(SimpleApp);
+    });
   });
 
-  it(`double .define() throws an error`, function() {
-    expect(() => customElements.define(`my-app`, SimpleApp))
-      .to.throw(`CustomElementRegistryError: 'my-app' name has already been registered`);
+  describe(`.define()`, function() {
+    it(`called twice throws an error`, function() {
+      expect(() => customElements.define(`my-app`, SimpleApp))
+        .to.throw(`Registration failed for type 'my-app'. A type with that name is already registered.`); });
   });
 });
