a9e56b9b6283ff4622c8bfdc64fbd0cdd1995cf9
Hans Li <hans.li@mixpanel.com>
2022-08-10 16:36:02 -0700

    Use chrome 75 to be capatible with wdio

--- karma.config.js
+++ karma.config.js
@@ -20,7 +20,7 @@ const sauceLaunchers = {
   sl_chrome_old: {
     base: `SauceLabs`,
     browserName: `chrome`,
-    browserVersion: `72`,
+    browserVersion: `75`,
     platformName: `macOS 10.13`,
     'sauce:options': {
       extendedDebugging: true,
@@ -104,10 +104,6 @@ module.exports = function (config) {
     browsers: SAUCE_LAB ? sauceBrowsers : [`ChromeHeadless`, `Firefox`],
     reporters: SAUCE_LAB ? [`spec`, `saucelabs`] : [`spec`],
     singleRun: true,
-    captureTimeout: 210000,
-    browserDisconnectTolerance: 3,
-    browserDisconnectTimeout: 210000,
-    browserNoActivityTimeout: 210000,
     customLaunchers: {
       ...sauceLaunchers,
       ChromeHeadless: {
