fd5282a016e2ecacffd198618e44e1001a6f83f5
Hans Li <hans.li@mixpanel.com>
2022-08-17 10:44:37 -0700

    Move component param test to shared iso folder to reduce redundancy

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,12 @@
+import serverNextAnimationFrame from './server/nextAnimationFrame';
+
 export function compactHtml(htmlStr) {
   return htmlStr.trim().replace(/>\s+</gm, `><`);
+}
+
+export function nextAnimationFrame() {
+  if (typeof requestAnimationFrame !== `undefined`) {
+    return new Promise((resolve) => requestAnimationFrame(resolve));
+  }
+  return serverNextAnimationFrame();
 }
