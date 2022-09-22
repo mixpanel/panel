888adec536eac6b47f7f9c1869e9eee305dcf5bb
Hans Li <hans.li@mixpanel.com>
2022-09-22 11:01:50 -0700

    Final comment adjustments

--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,5 @@
-import serverNextAnimationFrame from './server/nextAnimationFrame';
-
 export function compactHtml(htmlStr) {
   return htmlStr.trim().replace(/>\s+</gm, `><`);
 }
 
-export function nextAnimationFrame() {
-  if (typeof requestAnimationFrame !== `undefined`) {
-    return new Promise((resolve) => requestAnimationFrame(resolve));
-  }
-  return serverNextAnimationFrame();
-}
+export {default as nextAnimationFrame} from './server/nextAnimationFrame';
